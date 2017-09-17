bf906b9e6c3b650d41e02967e6b70383269dd696
didin jamaludin <didinj@gmail.com>
2017-09-17 17:33:05 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tutorial of Building Java REST API using Spring Boot and MongoDB
+
+This source code is part of [Tutorial of Building Java REST API using Spring Boot and MongoDB]()
---
+++ src/main/java/com/spring/restapi/repositories/ProductRepository.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.spring.restapi.repositories;
+
+import com.spring.restapi.models.Product;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author didin
+ */
+public interface ProductRepository extends CrudRepository<Product, String> {
+    @Override
+    Product findOne(String id);
+
+    @Override
+    void delete(Product deleted);
+}
